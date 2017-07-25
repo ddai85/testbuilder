91d0b960cf8388a9a66a34716288ae5318a51812
DanDai <ddai85@gmail.com>
2017-07-25 16:46:36 -0700

    Complete detectNetwork for all cards though until Maestro

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,13 +13,38 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-  if (cardNumber.length === 14){
-  	return 'Diner\'s Club';
-  }
-  if (cardNumber.length === 15){
-  	return 'American Express'
-  }
+  var cardArray = cardNumber.split('');
 
+  var dinerPrefix = cardArray[0] + cardArray[1];
+  if (cardNumber.length === 14 && (dinerPrefix === '38' || dinerPrefix === '39')){
+  	  return 'Diner\'s Club';
+  }
+  var amexPrefix = cardArray[0] + cardArray[1];
+  if (cardNumber.length === 15 && (amexPrefix === '34' || amexPrefix === '37')){
+  	  return 'American Express';
+  }
+  var visaPrefix = cardArray[0];
+  if (visaPrefix === '4' && (cardArray.length === 13 || cardArray.length === 16 || cardArray.length === 19)){
+  	  return 'Visa';
+  }
+  var masterPrefix = cardArray[0] + cardArray[1];
+  var masterArray = ['51', '52', '53', '54', '55'];
+  if ((masterArray.includes(masterPrefix)) && cardNumber.length === 16){
+  	return 'MasterCard';
+  }
+  var discoverPrefix = cardArray[0] + cardArray[1] + cardArray[2];
+  var discoverArray = ['644', '645', '646', '647', '648', '649'];
+  if (cardArray.length === 16 || cardArray.length === 19){
+  	if (cardArray[0] + cardArray[1] + cardArray[2] + cardArray[3] === '6011' || discoverArray.includes(discoverPrefix) || cardArray[0] + cardArray[1] === '65'){
+      return 'Discover';
+    }
+  }
+  var maestroPrefix = cardArray[0] + cardArray[1] + cardArray[2] + cardArray[3];
+  var maestroArray = ['5018', '5020', '5038', '6304'];
+  var maestroLength = [12, 13, 14, 15, 16, 17, 18, 19]
+  if (maestroArray.includes(maestroPrefix) || maestroLength.includes(cardNumber.length)){
+    return 'Maestro';
+  }
 };
 
-
+console.log(detectNetwork('5112345678901234'))
